d5c1b373d3c68708435c1338846516b885c29319
Shyam <shyam@drapcode.com>
2025-01-31 18:34:10 +0530

    added forget and reset password

--- router/userRoute.js
+++ router/userRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, getUser, loginUser } = require('../controller/user');
+const { createUser, getUser, loginUser, resetPassword, forgetPassword } = require('../controller/user');
 
 router.post("/createUser", createUser);
 router.post("/getUser", getUser);
 router.post("/loginUser", loginUser);
+router.post('/resetPassword/:token', resetPassword);
+router.post('/forgetPassword', forgetPassword);
 
 module.exports = router;
